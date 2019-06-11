86f4953b08c06ba2f37761e783e8c0aa1ed20cd6
Kwangchiu <c178810397@163.com>
2019-06-11 16:28:22 +0800

    结构初始化

--- MyNews/MyNews/OneViewController.m
+++ MyNews/MyNews/OneViewController.m
@@ -6,9 +6,15 @@
 //  Copyright © 2019年 Kwangchiu. All rights reserved.
 //
 
+
 #import "OneViewController.h"
+#import <MediaPlayer/MediaPlayer.h>
 
 @interface OneViewController ()
+
+/* 播放器 */
+@property (nonatomic, strong) MPMoviePlayerController *player;
+- (IBAction)play;
 
 @end
 
@@ -16,17 +22,28 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (MPMoviePlayerController *)player
+{
+    if (_player == nil) {
+        NSURL *url = [NSURL URLWithString:@"http://c.m.163.com/nc/video/home/0-20.html"];
+        
+        _player = [[MPMoviePlayerController alloc] initWithContentURL:url];
+        
+        // 设置控制器View所在的位置
+        _player.view.frame = CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.width * 9 / 16);
+        
+        // 设置播放器的控制模式
+        _player.controlStyle = MPMovieControlStyleFullscreen;
+        
+        [self.view addSubview:self.player.view];
+    }
+    return _player;
 }
-*/
+
+- (IBAction)play {
+    [self.player play];
+}
 
 @end
